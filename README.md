3ac5f5e60fb3e457b78f96d629c4e8087fee6776
Ramir Junior <junior.ribeiro0@live.com>
2021-05-29 23:32:16 -0300

    inicio dev week santander

---
+++ README.md
@@ -0,0 +1,5 @@
+# Semana Dev Week Santander
+
+Projeto desenvolvido durante a semana Dev Week Santander em parceria com a Digital Innovation One.
+
+- Status do projeto: Em andamento 
---
+++ app/src/main/java/com/examle/santanderdevweek/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.examle.santanderdevweek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
